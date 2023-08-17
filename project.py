188a4ece63d0a82afd88161d1413098db2567e32
kaynansn11@gmail.com <kaynansn11@gmail.com>
2023-08-17 14:36:43 -0300

    finished

--- project.py
+++ project.py
@@ -7,9 +7,11 @@ import concurrent.futures
 
 from bs4 import BeautifulSoup
 
+# global headers to be used for requests
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'}
 
 MAX_THREADS = 10
+
 
 def extract_movie_details(movie_link):
     time.sleep(random.uniform(0, 0.2))
@@ -19,17 +21,19 @@ def extract_movie_details(movie_link):
     if movie_soup is not None:
         title = None
         date = None
-
-        movie_data = movie_soup.find('div', attrs={'class': 'sc-385ac629-3 kRUqXl'})
+        # ajustar o trecho abaixo de acordo com o site de um filme, exemplo : https://www.imdb.com/title/tt15398776/?ref_=chtmvm_t_1, usar inspecionar elemento para definir os elementos.
+        movie_data = movie_soup.find('div', attrs={'class': 'sc-e226b0e3-3 jJsEuz'})
         if movie_data is not None:
+            # h1 deve ser o título do nome do filme
             title = movie_data.find('h1').get_text()
+            # date deve ser apenas a classe que representa o ano.
             date = movie_data.find('a', attrs={'class': 'ipc-link ipc-link--baseAlt ipc-link--inherit-color'}).get_text().strip()
-
+        # rating é a nota do filme, por exemplo, 8.6.
         rating = movie_soup.find('span', attrs={'class': 'sc-bde20123-1 iZlgcd'}).get_text() if movie_soup.find(
             'span', attrs={'class': 'sc-bde20123-1 iZlgcd'}) else None
-
-        plot_text = movie_soup.find('span', attrs={'class': 'sc-6a7933c5-1 fPmRoa'}).get_text().strip() if movie_soup.find(
-            'span', attrs={'class': 'sc-6a7933c5-1 fPmRoa'}) else None
+        # plot é o texto de sinopse do filme
+        plot_text = movie_soup.find('span', attrs={'class': 'sc-466bb6c-0 kJJttH'}).get_text().strip() if movie_soup.find(
+            'span', attrs={'class': 'sc-466bb6c-0 kJJttH'}) else None
 
         with open('movies.csv', mode='a') as file:
             movie_writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
@@ -39,6 +43,7 @@ def extract_movie_details(movie_link):
 
 
 def extract_movies(soup):
+    # aqui são configurações de hierarquia da página, do ponto de encontro do filme até sua divisão e organização em elementos.
     movies_table = soup.find('div', attrs={'data-testid': 'chart-layout-main-column'}).find('ul')
     movies_table_rows = movies_table.find_all('li')
     movie_links = ['https://imdb.com' + movie.find('a')['href'] for movie in movies_table_rows]
@@ -51,10 +56,12 @@ def extract_movies(soup):
 def main():
     start_time = time.time()
 
+    # IMDB Most Popular Movies - 100 movies
     popular_movies_url = 'https://www.imdb.com/chart/moviemeter/?ref_=nv_mv_mpm'
     response = requests.get(popular_movies_url, headers=headers)
     soup = BeautifulSoup(response.content, 'html.parser')
 
+    # Main function to extract the 100 movies from IMDB Most Popular Movies
     extract_movies(soup)
 
     end_time = time.time()
